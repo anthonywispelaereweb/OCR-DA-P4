49b7ca948302ec5d2c2bfad4e52628003378e625
anthony wispelaere <awispelaere@partner.auchan.fr>
2025-01-11 10:38:14 +0100

    add check form and add manager display error for each formDate

--- modal.js
+++ modal.js
@@ -33,6 +33,76 @@ modalbg.addEventListener("click", ()=> closeModal());
 modalBody.forEach((btn) => btn.addEventListener("click", (ev) => {
   ev.stopPropagation();
 }));
+
+const displayErrorMessage = (el, input, isError) => {
+  let idCase =  input.getAttribute("id") || input.getAttribute("name");
+  if(isError){
+    switch(idCase){    
+      case "first":
+        el.dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+        break;
+      case "last":
+        el.dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+        break;
+      case "email":
+        el.dataset.error = "Veuillez entrer une adresse email valide.";
+        break;
+      case "birthdate":
+        el.dataset.error = "Vous devez entrer votre date de naissance.";
+        break;
+      case "quantity":
+        el.dataset.error = "Veuillez entrer un nombre.";
+        break;
+      case "location1":
+        el.dataset.error = "Vous devez choisir une ville.";
+        break;
+      case "checkbox1":
+        el.dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
+        break;
+    }
+  } else {
+    el.dataset.error = ""
+  }
+}
+
+const checkFormValid = () => {
+  let isValid = true;
+  formData.forEach((data) => {
+    const input = data.querySelector("input");
+    if(input.value.trim() === ""){
+      data.dataset.errorVisible = true;
+      isValid = false;
+    } else {
+      data.dataset.errorVisible = false;
+    }
+    if(input.type === "radio" && input.name === "location"){
+      const radioName = input.getAttribute("name");
+      const radioChecked = document.querySelector(`input[name="${radioName}"]:checked`);
+      if(!radioChecked){
+        isValid = false;
+        data.dataset.errorVisible = true;
+      } else {
+        data.dataset.errorVisible = false;
+      }
+    }
+    if(input.type === "checkbox"){
+      if(!input.checked){
+        isValid = false;
+        data.dataset.errorVisible = true;
+      } else {
+        data.dataset.errorVisible = false;
+      }
+    }
+    displayErrorMessage(data, input, data.dataset.errorVisible)
+  });
+  return isValid;
+}
+
+
 formHTML.addEventListener("submit", (e) => {
   e.preventDefault();
+  const isValid = checkFormValid();
+  if(isValid){
+    console.log("Merci pour votre inscription");
+  }
 })
