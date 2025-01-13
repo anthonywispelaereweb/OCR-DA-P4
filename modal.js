bf1a9662f6c4f7a477e5a764fb51c3fc3acab51b
anthony wispelaere <awispelaere@partner.auchan.fr>
2025-01-13 16:11:05 +0100

    add js control and add comment

--- modal.js
+++ modal.js
@@ -2,7 +2,19 @@ const editNav = () => {
   let x = document.getElementById('myTopnav')
   x.classList.toggle('responsive')
 }
-
+// Add the max value for the birthdate input
+const initMaxValueBirthDate = () => {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+  const maxDate = `${year}-${month}-${day}` 
+  const birthdate = document.querySelector('#birthdate')
+  birthdate.setAttribute('max', maxDate)
+  console.log("🚀 ~ initMaxValueBirthDate ~ maxDate:", maxDate)
+  return maxDate
+}
+// initMaxValueBirthDate()
 // DOM Elements
 const modalbg = document.querySelector('.bground'),
   formHTML = document.querySelector('.modal-body form'),
@@ -13,6 +25,11 @@ const modalbg = document.querySelector('.bground'),
   navBarBtn = document.querySelector('.navbar-js'),
   firstInput = document.querySelector('#first');
 
+const globalData = {
+  maxBirthdate: initMaxValueBirthDate(),
+}
+
+// globalData.maxBirthdate = new Date().toISOString().split('T')[0]
 // launch modal form
 const launchModal = () => {
   modalbg.classList.add('show')
@@ -77,17 +94,47 @@ const displayErrorMessage = (el, input, isError) => {
     el.dataset.error = ''
   }
 }
-
+const validateEmail = (email) => {
+  // https://regexr.com/8autj
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 const checkFormValid = () => {
   let isValid = true
   formData.forEach(data => {
     const input = data.querySelector('input')
+    // manage empty input
     if (input.value.trim() === '') {
       data.dataset.errorVisible = true
       isValid = false
     } else {
       data.dataset.errorVisible = false
     }
+    // manage date input
+    if (input.type === 'date' && input.name === 'birthdate') {
+      const date = new Date(input.value)
+      const dateNow = new Date()
+      if (date > dateNow) {
+        isValid = false
+        data.dataset.errorVisible = true
+      } else {
+        data.dataset.errorVisible = false
+      }
+    }
+    // manage email input
+    if (input.type === 'email' && input.name === 'email') {
+      if (!validateEmail(input.value)) {
+        isValid = false
+        data.dataset.errorVisible = true
+      } else {
+        data.dataset.errorVisible = false
+      }
+    }
+
+    // manage radio input
     if (input.type === 'radio' && input.name === 'location') {
       const radioName = input.getAttribute('name')
       const radioChecked = document.querySelector(`input[name="${radioName}"]:checked`)
@@ -98,6 +145,8 @@ const checkFormValid = () => {
         data.dataset.errorVisible = false
       }
     }
+
+    // manage checkbox input
     if (input.type === 'checkbox') {
       if (!input.checked) {
         isValid = false
@@ -106,6 +155,7 @@ const checkFormValid = () => {
         data.dataset.errorVisible = false
       }
     }
+    // display error message or not
     displayErrorMessage(data, input, data.dataset.errorVisible)
   })
   return isValid
@@ -123,14 +173,14 @@ const postInfo = async() => {
     },
     body: JSON.stringify(data),
   }
-  console.log("🚀 ~ postInfo ~ options.data:", options.body)
-  const response = await fetch('https://restapi.fr/api/OCR-tes-tonio', options)
+
+  const response = await fetch(`https://restapi.fr/api/OCR-test-tonio-${globalData.maxBirthdate}`, options)
   if (response.ok) {
     console.log('🚀 ~ postInfo ~ response:', response)
     displaySuccessMessage()
-
   }
 }
+
 const displaySuccessMessage = () => {
   const hideElements = ['.formData', '.text-label', '.btn-submit']
   hideElements.forEach(el => {
